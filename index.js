708b2098f8319e59b93cb9ea82d6268676657bf7
Ben Noor <bennoor@Bens-MacBook-Pro.local>
2021-03-20 14:55:35 +1300

    Added algorithm for computer choice

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+// Main Computer Play Function
+
+function computerPlay() {
+    let computerChoice = Math.floor(Math.random() * (3 - 0) + 1)
+    switch(computerChoice) {
+        case 1:
+            computerChoice = 'Rock'
+            break;
+        case 2:
+            computerChoice = 'Paper'
+            break;
+        case 3:
+            computerChoice = 'Scissors'
+            break;
+    }
+    console.log(computerChoice);
+}
