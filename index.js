1999e0e1e0a7438a709f04ad0b1b907bcd529158
Ben Noor <bennoor@Bens-MacBook-Pro.local>
2021-03-20 15:08:18 +1300

    Added core algorithm for win/loss checks

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Main Computer Play Function
 
 function computerPlay() {
+    //Random selector with 1 as min, 3 as max
     let computerChoice = Math.floor(Math.random() * (3 - 0) + 1)
     switch(computerChoice) {
         case 1:
@@ -15,5 +16,23 @@ function computerPlay() {
             computerChoice = 'Scissors'
             break;
     }
-    console.log(computerChoice);
+    return computerChoice;
+}
+
+function playRound(playerChoice, computerPlay) {
+    if (playerChoice == computerPlay) {
+        console.log('Draw!')
+    } else if (playerChoice == 'Rock' && computerPlay == 'Paper') {
+        console.log('You Lose!')  
+    } else if (playerChoice == 'Rock' && computerPlay == 'Scissors') {
+        console.log('You Win!')
+    } else if (playerChoice == 'Paper' && computerPlay == 'Scissors'){
+        console.log('You Lose!')
+    } else if (playerChoice == 'Paper' && computerPlay == 'Rock') {
+        console.log("You Win!")
+    } else if (playerChoice == 'Scissors' && computerPlay == 'Rock') {
+        console.log("You Lose!")
+    } else if (playerChoice == 'Scissors' && computerPlay == 'Paper') {
+        console.log("You Win!")
+    }
 }
